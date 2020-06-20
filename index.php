4c5262116c72fc724c29581b811a1320f7ec1501
khaled azem <kalddevv@gmail.com>
2020-06-20 16:14:35 +0100

    change __DIR__ in autoload to check if it still works in production

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-   $dotenv->load();
- require_once "private/init.php";
-   
- $app = new App; //creates a new app instance
+$dotenv->load();
+var_dump($dotenv);
+require_once "private/init.php";
+$app = new App; //creates a new app instance
